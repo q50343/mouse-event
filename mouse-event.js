44f26ff0f40154ffd30d2b2585615b9ac7c20a71
q50343 <q503433@yahoo.com.tw>
2018-08-01 17:25:29 +0800

    mouse-event

---
+++ mouse-event.js
@@ -0,0 +1,37 @@
+var ball = document.querySelector('.ball');
+var arr = [];
+
+// ball move
+ball.onmousedown = function(e){
+
+    var grapX = e.offsetX;
+    var grapY = e.offsetY;
+
+    document.onmousemove = function (e){
+        var _left = e.clientX - grapX;
+        var _top = e.clientY - grapY;
+        ball.style.left = _left + 'px';
+        ball.style.top = _top + 'px';
+        arr.push({left:_left, top:_top});
+    }
+    ball.onmouseup = function (){
+        document.onmousemove = null
+    }
+}
+
+// return
+var btn = document.getElementById('button');
+btn.onclick = function(){
+    var index = arr.length - 1;
+    var timer = setInterval(function(){
+        
+        ball.style.left = arr[index].left+'px';
+        ball.style.top = arr[index].top+'px';
+
+        index--;
+        if(index < 0){
+            clearInterval(timer);
+            arr = [];
+        }
+    },30)
+}
